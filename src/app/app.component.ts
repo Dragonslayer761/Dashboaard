d99c5347321a3b386d0496c671393f111e1ad724
Subham Chowdhur <sc761851@gmail.com>
2021-11-26 19:41:29 +0530

    creating the main skelton of the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DashboardDetailsService } from './Services/dashboard-details.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bioinformusDashboard';
+  serverDetails:any;
+  filteredList:any;
+  entryValue:any = 10;
+  constructor(private dash:DashboardDetailsService){}
+  ngOnInit(){
+    this.getAllDetails();
+    this.filteredList=this.serverDetails
+  }
+  getAllDetails(){
+    this.dash.getServerDetails().subscribe(data =>{
+      this.serverDetails = data['db'];
+      this.filteredList=this.serverDetails
+    })
+  }
+  searchDomain(event:any){
+    let value= event.target.value.toLowerCase();
+    this.filteredList = this.serverDetails.filter((e:any) => e.domain.toLowerCase().includes(value))
+  }
+  getEntries(event:any){
+    console.log(event.target.value)
+    let value = event.target.value
+    if(value<this.serverDetails.length){
+      this.filteredList = this.serverDetails.slice(0,value)
+      console.log(this.filteredList)
+    }   
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerDeatialsComponent } from './Components/server-deatials/server-deatials.component';
+import { StorageIndicatorComponent } from './Components/storage-indicator/storage-indicator.component';
+import { ServerStatusComponent } from './Components/server-status/server-status.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerDeatialsComponent,
+    StorageIndicatorComponent,
+    ServerStatusComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
